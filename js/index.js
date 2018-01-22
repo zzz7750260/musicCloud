c60199c77888d22ce2f787a1601558f676ec5279
zzz7750260 <547926016@qq.com>
2018-01-22 23:22:43 +0800

    添加历史歌词的相关class和样式

--- js/index.js
+++ js/index.js
@@ -512,7 +512,7 @@ function musicHistory(tjson){
 		$.each(asongFolderA,function(index,item){
 			console.log("这个是数组获取")
 			console.log(item[index])
-			$(".the_collect_history_k").find("ul").append("<li class ='li-muisic-"+item.sid+"' data-iclass='"+item.sid+"' data-surl='"+item.url+"'><i class='iconfont icon-bofangqibofang iconfont-i-"+item.sid+"' ></i><span>"+item.title+"</span><span>"+item.artist+"</span></li>")		
+			$(".the_collect_history_k").find("ul").append("<li class ='li-muisic-"+item.sid+" li_music ' data-iclass='"+item.sid+"' data-surl='"+item.url+"'><i class='iconfont icon-bofangqibofang iconfont-i-"+item.sid+" li_music_i' ></i><span class='li_music_c'>"+item.title+"</span><span class='li_music_a'>"+item.artist+"</span></li>")		
 		})
 		
 	}
